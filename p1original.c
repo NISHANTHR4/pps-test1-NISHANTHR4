6681fe56d8798f4b99b5014c9aaac726b38e89b8
NISHANTHR4 <97089406+NISHANTHR4@users.noreply.github.com>
2022-01-19 13:10:52 +0000

    p1original

--- p1original.c
+++ p1original.c
@@ -1,2 +1,10 @@
-
+#include<stdio.h>
+int main()
+{
+  int num1,num2,sum;
+  printf("Enter two numbers\n");
+  scanf("%d%d",&num1,&num2);
+  sum=num1+num2;
+  printf("Addition of %d and %d is %d\n", num1,num2,sum);
+  return 0;
 }
